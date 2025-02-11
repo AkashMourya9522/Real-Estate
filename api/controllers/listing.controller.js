de445e58deb7e2da5853a0fd60b96ae44f521318
Akash Mourya <akashmourya9522@gmail.com>
2025-02-11 23:14:34 +0530

    Created Update Listing API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,32 +1,56 @@
-import Listing from "../models/listing.model.js"
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createListing = async(req,res,next)=>{
-    try {
-        const listing = await Listing.create(req.body);
-        console.log(listing);
-        
-        return res.status(200).json({success:true,data:listing})
-    } catch (error) {
-        next(error)
-    }
-}
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    console.log(listing);
 
-export const deleteListing = async (req,res,next)=>{
-    
-    try{
-        const listing = await Listing.findById(req.params.id)
-        if(listing){
-            if(listing.userRef != req.userId){
-                return next(errorHandler("Error in deleting(You can delete Your own listings)",404))
-            }
-            const dbRes = await Listing.findByIdAndDelete(listing._id)
-            res.status(200).json({success:true,message:"Listing Has Been Deleted"})
-        }else{
-            return next(errorHandler("Error ",404))
-        }
-    }catch(err){
-        return next(err)
+    return res.status(200).json({ success: true, data: listing });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (listing) {
+      if (listing.userRef != req.userId) {
+        return next(
+          errorHandler(
+            "Error in deleting(You can delete Your own listings)",
+            404
+          )
+        );
+      }
+      const dbRes = await Listing.findByIdAndDelete(listing._id);
+      res
+        .status(200)
+        .json({ success: true, message: "Listing Has Been Deleted" });
+    } else {
+      return next(errorHandler("Error ", 404));
     }
-   
-}
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const editListing = async (req, res, next) => {
+  let listingId = req.params.id;
+  try {
+    let listing = await Listing.findById(listingId);
+    if (!listing) {
+      return next(errorHandler("Listing Missing", 404));
+    }
+    if (listing.userRef != req.userId) {
+      return next(errorHandler("Invalid Access", 404));
+    }
+    let updatedListing = await Listing.findByIdAndUpdate(listingId, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedListing);
+  } catch (err) {
+    next(err);
+  }
+};
