f7bd80b2785c12fbe0b8d82afb432bb1cea6a29a
Akash Mourya <akashmourya9522@gmail.com>
2025-02-20 18:38:56 +0530

    Completed Home Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="text-lg max-w-6xl mx-auto  py-10 px-5">
+      <h1 className="text-slate-700 text-3xl font-semibold mb-5">Welcome To Akash Estates</h1>
+      <p className="text-slate-800 mb-5">
+        {" "}
+        At AkashEstates, we specialize in providing personalized real
+        estate solutions that help our clients achieve their property goals with
+        ease and confidence. Whether you're buying, selling, or investing, our
+        team is dedicated to delivering expert guidance and exceptional service
+        every step of the way. With years of experience in the local market, we
+        take pride in offering in-depth knowledge and a tailored approach to
+        meet your unique needs.
+      </p>
+      <p className="text-slate-800 mb-5">
+        Founded in 2025, we’ve built a strong reputation for our unwavering
+        commitment to honesty, integrity, and client satisfaction. We understand
+        that real estate decisions are some of the most significant in your
+        life, and we’re here to ensure that your experience is smooth,
+        stress-free, and successful. From first-time homebuyers to seasoned
+        investors, we offer professional support at every stage of your real
+        estate journey.
+      </p>
+      <p className="text-slate-800 mb-5">
+        What sets us apart is our client-focused approach. We don’t just want to
+        make a sale; we want to build lasting relationships and ensure that your
+        real estate goals are met with precision and care. Whether you're buying
+        your first home, upgrading to a new one, or seeking an investment
+        opportunity, our team is here to help you make informed decisions and
+        achieve the results you deserve.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
